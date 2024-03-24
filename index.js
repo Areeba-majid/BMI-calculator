a21b2e1130913011292d187a25bcd428ea34b1bc
Areeba Riaz <160718011+Areeba-majid@users.noreply.github.com>
2024-03-24 06:16:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+//BMI Calculator in typescript
+let weightInKg = 45;
+let heightInMeters = 1.58;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`your BMI is ${bmi}`);
+export {};
---
+++ index.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer"
+
+//BMI Calculator in typescript
+
+let weightInKg = 45;
+let heightInMeters = 1.58;
+let bmi = weightInKg / (heightInMeters * heightInMeters)
+
+console.log(`your BMI is ${bmi}`)
